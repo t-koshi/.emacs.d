f30b8193ce9d62955e57aca59a893d897907a305
t-koshi <greapflutea2@yahoo.co.jp>
2019-02-26 23:50:13 +0900

    modify README

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 - `brew install cask`
 - `brew install multimarkdown`
 
+### 初期化コマンド
+- `cd ~/.emacs.d`
+- `cask install`
+
 ### パッケージのインストール方法
 - `M-x pallet-install`
 - `M-x pallet-update`
--- init.el
+++ init.el
@@ -16,6 +16,12 @@
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
 (define-key company-active-map (kbd "C-h") nil)
 
+;;; テーマの設定
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(load-theme 'solarized t)
+(set-frame-parameter nil 'background-mode 'dark)
+(set-terminal-parameter nil 'background-mode 'dark)
+
 (defun company--insert-candidate2 (candidate)
   (when (> (length candidate) 0)
     (setq candidate (substring-no-properties candidate))
@@ -35,5 +41,24 @@
         (company-complete-selection)
       (company--insert-candidate2 company-common))))
 
+(defadvice markdown-open (before my-markdown-save-and-open activate)
+  "Save buffer before open file for the current buffer."
+  (unless (buffer-file-name) (user-error "Must be visiting a file"))
+  (save-buffer (buffer-file-name)))
+
 (define-key company-active-map [tab] 'company-complete-common2)
 (define-key company-active-map [backtab] 'company-select-previous) ; おまけ
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit lsp-ui lsp-ruby idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell drag-stuff company-lsp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
